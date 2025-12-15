a0973ba5d6441742741df38883557a59885c2d88
Matys Eyheraguibel <matys.eyheraguibel@etu.minesparis.psl.eu>
2025-12-15 21:16:05 +0100

    linkedlisttp 2

--- linkedlist-tp.cpp
+++ linkedlist-tp.cpp
@@ -50,6 +50,9 @@
 
 // Voici un exemple de main qui doit fonctionner avec votre liste chaînée:
 
+#include <iostream>
+#include <cstdlib>
+
 struct Cell {
 int val;
 Cell *as; //as pour adresse suivant
@@ -60,7 +63,7 @@ struct LinkedList {
   Cell *cell0;
   LinkedList():cell0(nullptr) {}
 
-  void is_empty() {
+  bool is_empty() {
   return cell0==nullptr ;
   }
 
@@ -71,9 +74,45 @@ struct LinkedList {
   }
 
   void print() {
-    
-
+    Cell * pointeurtemp=cell0 ;
+    while (pointeurtemp!=nullptr) { 
+      
+      std::cout<<(*pointeurtemp).val<<std::endl ; 
+      pointeurtemp=(*pointeurtemp).as;
+    }
+  
   }
+
+   bool find (int t) 
+  {
+    Cell * pointeurtemp=cell0 ;
+    while (pointeurtemp==nullptr && pointeurtemp->val!=t) { 
+      pointeurtemp=(*pointeurtemp).as;
+    }
+    return pointeurtemp!= nullptr ;
+  }
+
+    void remove (int t) 
+  {
+    //cas empty à faire
+    //cas ou find renvoie false
+    //pas fini
+    //je comprends pas trop les ->
+
+
+    if (cell0->val == t) { 
+      cell0 = cell0->as; 
+       return; } 
+         
+    Cell * pointeurtemp=cell0 ;
+    while (pointeurtemp->val!=t) { 
+      pointeurtemp=(*pointeurtemp).as;
+    }
+    Cell * pointeurav=pointeurtemp ;
+    pointeurtemp=(*pointeurtemp).as;
+    pointeurav->as=pointeurtemp->as ;
+  }
+
 
 };
 
@@ -82,7 +121,18 @@ int main () {
 
     list.push_front(45);
     list.push_front(12);
-    
+    list.push_front(42);
+    list.push_front(33);
+    list.push_front(17);
 
+    list.print(); // Affiche: 17 33 42 12 45
+                  // oui l'ordre est inversé car on ajoute en tête de liste...
+    list.remove(88); // Ne fait rien (et pas d'exception à générer)
+    list.remove(17); // Attention on supprime la tête de liste !!
+    list.remove(45); // Attention on supprime la queue de liste !!
+    list.remove(42); // Supprime un élément du milieu de liste
+    // que de cas à gérer... que de pointeurs à mettre à jour...
+
+    list.print();
     return 0;
 }
